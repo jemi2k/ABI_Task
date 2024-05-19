57d4a01940b88dd1dd42f68a8942beebdc085353
Ermias <81765649+jemi2k@users.noreply.github.com>
2024-05-19 12:31:48 -0700

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -1,1 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor() ERC20("MyToken", "MT") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        super.transfer(recipient, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) public override returns (bool) {
+        super.approve(spender, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+        super.transferFrom(sender, recipient, amount);
+        return true;
+    }
+}
 
